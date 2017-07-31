41bc8ebea4ba1d2aac29b0c6b77410c2f0ac9ec0
See Yam Lim <slim@Sees-MBP.domain>
2017-07-31 16:02:12 -0700

    Initial commit

---
+++ src/main/java/com/syamlim/seethings/SeethingsApplication.java
@@ -0,0 +1,12 @@
+package com.syamlim.seethings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeethingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeethingsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/syamlim/seethings/SeethingsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.syamlim.seethings;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SeethingsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
