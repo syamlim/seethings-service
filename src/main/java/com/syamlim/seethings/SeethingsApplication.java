23c0b9400ae0ea03daa6ea7d07398b981c528196
Seeyam Lim <syamlim@gmail.com>
2024-04-28 11:23:19 -0700

    test commit

--- src/main/java/com/syamlim/seethings/SeethingsApplication.java
+++ src/main/java/com/syamlim/seethings/SeethingsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SeethingsApplication {
 
 	public static void main(String[] args) {
+                #add comment here
 		SpringApplication.run(SeethingsApplication.class, args);
 	}
 }
